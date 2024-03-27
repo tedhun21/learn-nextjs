67191fde9b52ef6121cd89f66baa556d5267645f
JiHun <82702997+tedhun21@users.noreply.github.com>
2024-03-27 14:19:01 +0900

    first commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Home",
+};
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+async function getMovies() {
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
+  const json = await response.json();
+
+  return json;
+}
+export default async function HomePage() {
+  const movies = await getMovies();
+
+  return (
+    <div>
+      <h1>
+        {movies.map((movie) => (
+          <li>
+            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </h1>
+    </div>
+  );
+}
